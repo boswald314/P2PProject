a9587533d0f62a5213f90f3eb9f564a86b3942d9
Bryan Oswald <boswald@Bryans-Mac-Pro.local>
2015-12-08 23:10:04 -0600

    created Graph class

--- finalProjectCS2.py
+++ finalProjectCS2.py
@@ -40,163 +40,167 @@ import matplotlib.pyplot as plt
 import time
 
 
-	
-def randomWalk(graph):
-	'''
-		Performs random walk to find target node
-			takes graph object as single parameter
-		Returns tuple:
-			(number of nodes visited, time elapsed)
-			if node cannot be found, both values are returned 0
-	'''
-	numberOfNodes = graph.number_of_nodes() -1
-	startNode = random.randint(0, numberOfNodes)
-	nodesVisited = 1
-	start = time.time()
-	while (graph.node[startNode]['targetNode'] == False):
-		graph.node[startNode]['visited'] = True
-		possibleNextStep = graph.neighbors(startNode)
-		if len(possibleNextStep) == 0:
-			return (0,0)
-		startNode = random.choice(possibleNextStep)
-		while graph.node[startNode]['visited'] == True:
-			possibleNextStep.remove(startNode)
-			if len(possibleNextStep) != 0:
-				startNode = random.choice(possibleNextStep)
+
+
+class Graph:
+	def __init__(self, n, p):
+		self.graph = nx.erdos_renyi_graph(n,p)
+
+	def isConnected(self):
+		return nx.is_connected(self.graph)
+
+	def getRandomNode(self):
+		nodes = self.graph.number_of_nodes()
+		return self.graph.node[random.randint(0, nodes - 1)]
+
+	def number_of_nodes(self):
+		return nx.number_of_nodes(self.graph)
+
+	def diamDist(self, n, minp, maxp):
+		number = maxp - minp
+		xList = []
+		yList = []
+		#iterations = 0
+		while p <= 0.05:
+			#print(p)
+			graph = nx.erdos_renyi_graph(n, p)
+			if self.isConnected == True:
+				graphDiameter = nx.diameter(self.graph)
+				yList.append(p)
+				xList.append(graphDiameter)
+				print("The diameter is: "+ str(graphDiameter))
 			else:
+				print("Diameter cannot be found")
+			p += number/100
+
+		plt.title('Diameter Distribution Graph')
+		plt.xlabel('Diameter')
+		plt.ylabel('Probability')
+			
+		plt.scatter(xList,yList)
+		#plt.show()
+		plt.savefig("DiameterDistributionGraph.png")
+
+	def initializeGraph(self, popd):
+		nx.set_node_attributes(self.graph, "targetNode", False)
+		nx.set_node_attributes(self.graph, "visited", False)
+
+		numberOfObjects = int(self.graph.number_of_nodes() * popd)
+		for i in range(numberOfObjects):
+			node = self.getRandomNode()
+			node['targetNode'] = True
+
+	def randomWalk(self):
+		'''
+			Performs random walk to find target node
+				takes graph object as single parameter
+			Returns tuple:
+				(number of nodes visited, time elapsed)
+				if node cannot be found, both values are returned 0
+		'''
+		numberOfNodes = self.graph.number_of_nodes() 
+		startNode = random.randint(0, numberOfNodes - 1)
+		nodesVisited = 1
+		start = time.time()
+		while (self.graph.node[startNode]['targetNode'] == False):
+			self.graph.node[startNode]['visited'] = True
+			possibleNextStep = self.graph.neighbors(startNode)
+			if len(possibleNextStep) == 0:
 				return (0,0)
-				
-		nodesVisited +=1
-
-	end = time.time()
-	elapsedtime = end - start
-	return (nodesVisited,elapsedtime)
-	  
-
-
-def kRandomWalk(graph,k=5):
-	'''
-		Performs k random walk to find target node
-			takes graph object as first parameter
-			takes k value (correspoding to number of random walkers) as second parameter
-		Returns tuple:
-			(number of nodes visited, time elapsed, hops made (by each walker, not total))
-			if node cannot be found, all values are returned 0
-	'''
-	numberOfNodes = graph.number_of_nodes()
-	startNodes = [0]*k
-	for i in range(0,k):
-		nodeToAdd = random.randint(0, numberOfNodes - 1)
-		while nodeToAdd in startNodes[:i]:
-			nodeToAdd = random.randint(0, numberOfNodes - 1)
-		startNodes[i] = nodeToAdd
-	nodesVisited = 1
-	hopsBeforeFound = 0
-	start = time.time()
-	while True:
-		index = 0
-		for i in startNodes:
-			graph.node[i]['visited'] = True
-			if graph.node[i]['targetNode'] == True:
-				end = time.time()
-				return (nodesVisited,end-start,hopsBeforeFound)
-			else:
-				possibleNextStep = graph.neighbors(i)
-
-				if len(possibleNextStep) == 0:
-					return (0,0,0)
-
-				for item in possibleNextStep:
-					if graph.node[item]['targetNode'] == True:
-						end = time.time()
-						return (nodesVisited,end-start,hopsBeforeFound)
-				startNode = random.choice(possibleNextStep)
-				while graph.node[startNode]['visited'] == True:
-					possibleNextStep.remove(startNode)
-					if len(possibleNextStep) != 0:
-						startNode = random.choice(possibleNextStep)
-					else:
-						return (0,0,0)
-				startNodes[index] = startNode
-				index +=1
-				nodesVisited +=1
-		hopsBeforeFound +=1
-
-
-
-
-def gnutellaFlooding(graph, ttl=7):
-	'''
-		Performs gnutella flooding to find target node
-			takes graph object as first parameter
-			second parameter is ttl of flood, defaults to 7
-		Returns tuple:
-			(number of nodes visited, time elapsed)
-			if node cannot be found, both values are returned 0
-	'''
-	numberOfNodes = graph.number_of_nodes()
-	nodesToCheck = [random.randint(0, numberOfNodes - 1)]
-	nodesVisited = 1
-	
-	start = time.time()
-	while True:
-		if (nodesVisited > ttl):
-			return(0, 0)
-		for i in nodesToCheck:
-			graph.node[i]['visited'] = True
-			if graph.node[i]['targetNode'] == True:
-				end = time.time()
-				return (nodesVisited, end-start)
-			else:
-				neighbors = graph.neighbors(i)
-				for item in neighbors:
-					if graph.node[item]['visited'] == False:
-						nodesToCheck.append(item)
-				nodesToCheck.remove(i)
-				if len(nodesToCheck) == 0:
+			startNode = random.choice(possibleNextStep)
+			while self.graph.node[startNode]['visited'] == True:
+				possibleNextStep.remove(startNode)
+				if len(possibleNextStep) != 0:
+					startNode = random.choice(possibleNextStep)
+				else:
 					return (0,0)
-			nodesVisited += 1
+					
+			nodesVisited +=1
+
+		end = time.time()
+		elapsedtime = end - start
+		return (nodesVisited,elapsedtime)
 
 
+	def kRandomWalk(self,k=5):
+		'''
+			Performs k random walk to find target node
+				takes graph object as first parameter
+				takes k value (correspoding to number of random walkers) as second parameter
+			Returns tuple:
+				(number of nodes visited, time elapsed, hops made (by each walker, not total))
+				if node cannot be found, all values are returned 0
+		'''
+		numberOfNodes = self.graph.number_of_nodes()
+		startNodes = [0]*k
+		for i in range(0,k):
+			nodeToAdd = random.randint(0, numberOfNodes - 1)
+			while nodeToAdd in startNodes[:i]:
+				nodeToAdd = random.randint(0, numberOfNodes - 1)
+			startNodes[i] = nodeToAdd
+		nodesVisited = 1
+		hopsBeforeFound = 0
+		start = time.time()
+		while True:
+			index = 0
+			for i in startNodes:
+				self.graph.node[i]['visited'] = True
+				if self.graph.node[i]['targetNode'] == True:
+					end = time.time()
+					return (nodesVisited,end-start,hopsBeforeFound)
+				else:
+					possibleNextStep = self.graph.neighbors(i)
 
+					if len(possibleNextStep) == 0:
+						return (0,0,0)
 
+					for item in possibleNextStep:
+						if self.graph.node[item]['targetNode'] == True:
+							end = time.time()
+							return (nodesVisited,end-start,hopsBeforeFound)
+					startNode = random.choice(possibleNextStep)
+					while self.graph.node[startNode]['visited'] == True:
+						possibleNextStep.remove(startNode)
+						if len(possibleNextStep) != 0:
+							startNode = random.choice(possibleNextStep)
+						else:
+							return (0,0,0)
+					startNodes[index] = startNode
+					index +=1
+					nodesVisited +=1
+			hopsBeforeFound +=1
 
-def diamDist(n, minp, maxp):
-	number = maxp - minp
-	xList = []
-	yList = []
-	#iterations = 0
-	while p <= 0.05:
-		#print(p)
-		graph = nx.erdos_renyi_graph(n, p)
-		if nx.is_connected(graph1) == True:
-			graphDiameter = nx.diameter(graph1)
-			yList.append(p)
-			xList.append(graphDiameter)
-			print("The diameter is: "+ str(graphDiameter))
-		else:
-			print("Diameter cannot be found")
-		p += number/100
-
-	plt.title('Diameter Distribution Graph')
-	plt.xlabel('Diameter')
-	plt.ylabel('Probability')
+	def gnutellaFlooding(self, ttl=7):
+		'''
+			Performs gnutella flooding to find target node
+				takes graph object as first parameter
+				second parameter is ttl of flood, defaults to 7
+			Returns tuple:
+				(number of nodes visited, time elapsed)
+				if node cannot be found, both values are returned 0
+		'''
+		numberOfNodes = self.graph.number_of_nodes()
+		nodesToCheck = [random.randint(0, numberOfNodes - 1)]
+		nodesVisited = 1
 		
-	plt.scatter(xList,yList)
-	#plt.show()
-	plt.savefig("DiameterDistributionGraph.png")
-
-
-def initializeGraph(graph,popd):
-	nx.set_node_attributes(graph, "targetNode", False)
-	nx.set_node_attributes(graph, "visited", False)
-
-	numberOfObjects = int(graph.number_of_nodes() * popd)
-	for i in range(numberOfObjects):
-		node = getRandomNode(graph)
-		node['targetNode'] = True
-
-	return graph
+		start = time.time()
+		while True:
+			if (nodesVisited > ttl):
+				return(0, 0)
+			for i in nodesToCheck:
+				self.graph.node[i]['visited'] = True
+				if self.graph.node[i]['targetNode'] == True:
+					end = time.time()
+					return (nodesVisited, end-start)
+				else:
+					neighbors = self.graph.neighbors(i)
+					for item in neighbors:
+						if self.graph.node[item]['visited'] == False:
+							nodesToCheck.append(item)
+					nodesToCheck.remove(i)
+					if len(nodesToCheck) == 0:
+						return (0,0)
+				nodesVisited += 1
 
 
 
@@ -210,14 +214,14 @@ def run():
 
 
 	#generate a number of random erdos renyi graphs of sizes and prob from 0.0001 to 0.05
-	FACTOR = 100
+	FACTOR = 1
 
 	p = 0
 	n = 5 * FACTOR
 	ppList = []
 	while p <= 0.05 :
 		p += 0.0005
-		graphs500.append(nx.erdos_renyi_graph(n,p))
+		graphs500.append(Graph(n,p))
 		ppList.append(p)
 		
 
@@ -225,19 +229,19 @@ def run():
 	n = 10 * FACTOR
 	while p <= 0.05 :
 		p += 0.0005	
-		graphs1000.append(nx.erdos_renyi_graph(n,p))   
+		graphs1000.append(Graph(n,p))   
 
 	p = 0
 	n = 20 * FACTOR
 	while p <= 0.05 :
 		p += 0.0005
-		graphs2000.append(nx.erdos_renyi_graph(n,p))
+		graphs2000.append(Graph(n,p))
 
 	p = 0
 	n = 50 * FACTOR
 	while p <= 0.05 :
 		p += 0.0005
-		graphs5000.append(nx.erdos_renyi_graph(n,p))
+		graphs5000.append(Graph(n,p))
 
 
 	res500 = []
@@ -263,27 +267,28 @@ def run():
 		#going through the 100 graphs for each 500,1000,2000,5000
 		for graph in graphlist:
 			
-			nx.set_node_attributes(graph, "targetNode", False)
-			nx.set_node_attributes(graph, "visited", False)
+			nx.set_node_attributes(graph.graph, "targetNode", False)
+			nx.set_node_attributes(graph.graph, "visited", False)
 
 			#base population density
 			density = 0.001
 			while density <= .01:
-				nx.set_node_attributes(graph, "targetNode", False)
+				nx.set_node_attributes(graph.graph, "targetNode", False)
 				numberOfNodes = graph.number_of_nodes()
 				for i in range(int(numberOfNodes*density)):
-					graph.node[random.randint(0,numberOfNodes-1)]['targetNode'] = True
-				res = randomWalk(graph)
+					node = graph.getRandomNode()
+					node['targetNode'] = True
+				res = graph.randomWalk()
 				#print(res)
 				RW_res.append(res)
 
-				nx.set_node_attributes(graph, "visited", False)
-				kRW_res.append(kRandomWalk(graph,5))
+				nx.set_node_attributes(graph.graph, "visited", False)
+				kRW_res.append(graph.kRandomWalk(5))
 
-				nx.set_node_attributes(graph, "visited", False)
-				flood_res.append(gnutellaFlooding(graph))
+				nx.set_node_attributes(graph.graph, "visited", False)
+				flood_res.append(graph.gnutellaFlooding())
 
-				nx.set_node_attributes(graph, "visited", False)
+				nx.set_node_attributes(graph.graph, "visited", False)
 				density += 0.001
 				density = float(str(density)[:6])
 
@@ -382,14 +387,6 @@ def run():
 		X_Values_time = []
 
 
-def getRandomNode(graph):
-	nodes = graph.number_of_nodes()
-	return graph.node[random.randint(0, nodes - 1)]
-
-
-
-
-
 
 
 def test(n=1000,p=0.01,popd=0.005):
@@ -397,19 +394,17 @@ def test(n=1000,p=0.01,popd=0.005):
 	p = float(p)
 	popd = float(popd)
 
-	graph = nx.erdos_renyi_graph(n,p)
-	graph = initializeGraph(graph, popd)
+	graph = Graph(n,p)
+	graph.initializeGraph(popd)
 
 	print("The graph contained {} nodes, was generated with p={}, and has an object density of {}".format(n,p,popd))
-	res = randomWalk(graph)
+	res = graph.randomWalk()
 	print("The random walker visited {} nodes and took {} seconds".format(res[0],res[1]))
-	nx.set_node_attributes(graph, "visited", False)
-	res = kRandomWalk(graph,5)
+	nx.set_node_attributes(graph.graph, "visited", False)
+	res = graph.kRandomWalk(5)
 	print("The k random walkers visited {} nodes over {} seconds and {} rounds of hops".format(res[0],res[1],res[2]))
-	res = gnutellaFlooding(graph)
+	res = graph.gnutellaFlooding()
 	print("The gnutella flood sent {} queries and took {} seconds\n".format(res[0],res[1]))
-
-
 
 def listcommands(cmd=''):
 	print("Commands available:")
@@ -449,10 +444,4 @@ if __name__ == "__main__":
 
 
 
-
-
-
-
-
-
-
+			
